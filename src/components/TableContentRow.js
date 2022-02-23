117e4235e9212f5ae2235ee42261e060069fceff
Reynold Osei Adade <reynold@walulel.com>
2022-02-23 15:25:29 +0000

    text styles for list

--- src/components/TableContentRow.js
+++ src/components/TableContentRow.js
@@ -1,10 +1,11 @@
 import NextCaret from "../images/next.svg";
+import moment from "moment";
 const TableRow = ({ testimonials }) => {
   return (
     <div className="w-full h-[640px] border-t">
       {testimonials.map((testimonial) => (
         <div
-          className="flex justify-between items-center h-[64px]"
+          className="flex justify-between items-center h-[64px] hover:bg-background border-b"
           key={testimonials.id}
         >
           <div className="inline-flex w-1/3 items-center">
@@ -22,19 +23,28 @@ const TableRow = ({ testimonials }) => {
                 className="h-[42px] w-[42px] rounded-full"
               />
             </div>
-            <div className="text-left">
-              <div>{testimonial.mentor.handle}</div>
+            <div className="text-left ">
+              <div className="font-semibold text-light-default">
+                {testimonial.mentor.handle}
+              </div>
               <div>
-                <span>
+                <span className="text-light-secondary text-[14px]">
                   on {testimonial.exercise.title} in {testimonial.track.title}
                 </span>
               </div>
             </div>
           </div>
           <div className="w-[513px]  h-[26px] truncate">
-            <span className="w-full">{testimonial.content}</span>
+            <span className="w-full text-light-default text-[15px]">
+              {testimonial.content}
+            </span>
           </div>
           <div className="flex-grow flex justify-end items-center">
+            <div>
+              <span className="text-light-secondary text-[14px]">
+                {moment(testimonial.created_at, "YYYYMMDD").fromNow()}
+              </span>
+            </div>
             <img
               src={NextCaret}
               alt="view details"
