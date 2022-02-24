cb554337b0cd505f4fe1a05b0d0d168a5d2af674
Reynold Osei Adade <reynold@walulel.com>
2022-02-24 10:26:27 +0000

    setup store

--- src/components/PageNumbers.js
+++ src/components/PageNumbers.js
@@ -5,36 +5,55 @@ export default function Paginator({ paginationData }) {
   const [currentPages, setCurrentPages] = useState([]);
   useEffect(() => {
     setCurrentPages(
-      // computePages(paginationData.current_page, paginationData.total_pages)
-      computePages(5, 300)
+      computePages(paginationData.current_page, paginationData.total_pages)
     );
   }, [paginationData.current_page, paginationData.total_pages]);
-  // console.log("current pages", currentPages);
-  // console.log
 
-  function computePages(currentPage, total_pages) {
-    console.log(currentPage, total_pages);
+  function computePages(current_page, total_pages) {
     let pages = [];
-    let start = currentPage - 2;
-    let end = currentPage + 2;
-    if (start < 1) {
+    let start = current_page - 2;
+    let end = current_page + 2;
+
+    if (total_pages < 6) {
       start = 1;
-      end = 5;
-    }
-    if (end > total_pages) {
       end = total_pages;
-      start = total_pages - 4;
+      for (let i = start; i <= end; i++) {
+        pages.push(i);
+      }
+      return pages;
     }
+
+    if (start < 3) {
+      start = 1;
+      for (let i = start; i <= end; i++) {
+        pages.push(i);
+      }
+      pages.push("...", total_pages);
+      return pages;
+    }
+
+    if (end > total_pages - 2) {
+      end = total_pages;
+      pages.push(1, "...");
+      for (let i = start; i <= end; i++) {
+        pages.push(i);
+      }
+      return pages;
+    }
+
+    pages.push(1, "...");
     for (let i = start; i <= end; i++) {
       pages.push(i);
     }
+    pages.push("...", total_pages);
+
     return pages;
   }
 
   return (
     <div className="flex">
-      {currentPages.map((page) => {
-        return <PageButton page={page} />;
+      {currentPages.map((page, index) => {
+        return <PageButton page={page} key={page + index} />;
       })}
     </div>
   );
